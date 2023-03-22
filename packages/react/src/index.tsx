2329bc14218beefd974edb9e35788cd307d56f87
juanevangelista1 <juan.evangelista.nascimentoo@gmail.com>
2023-03-21 22:11:54 -0300

    Commit Final

--- packages/react/src/index.tsx
+++ packages/react/src/index.tsx
@@ -1,14 +1,11 @@
-import { styled } from "./styles";
+export * from './components/Box'
+export * from './components/Text'
+export * from './components/Heading'
+export * from './components/Avatar'
+export * from './components/Button'
+export * from './components/TextInput'
+export * from './components/TextArea'
+export * from './components/Checkbox'
+export * from './components/MultiStep'
 
-const Button = styled("button", {
-  fontFamily: "$default",
-  backgroundColor: "$ignite300",
-});
-
-export function App() {
-  return (
-    <>
-      <Button>Hey World</Button>
-    </>
-  );
-}
+export * from './styles'
